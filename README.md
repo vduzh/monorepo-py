916ad9117d904306e7126d744fc37deb659053a0
vduzh <vduzh@tut.by>
2024-02-12 13:25:04 +0300

    Learned the openai lib

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 $ where poetry
 C:\Users\user\AppData\Local\Programs\Python\Python39\Scripts\poetry.exe
 
-# commands 
+# commands
+https://gist.github.com/CarlosDomingues/b88df15749af23a463148bd2c2b9b3fb
+poetry add <library>
 poetry build
---
+++ libs/openai/openai_test.py
@@ -0,0 +1,56 @@
+import os
+import unittest
+
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+model = "gpt-3.5-turbo"
+model_with_embedding = "text-embedding-3-small"
+
+
+class TestOpenAI(unittest.TestCase):
+
+    def test_create_completion(self):
+        test_value = "This is a test!"
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Say " + test_value,
+                }
+            ],
+            model=model,
+        )
+        self.assertTrue(len(chat_completion.choices), 1)
+
+        choice = chat_completion.choices[0]
+        self.assertTrue(choice.finish_reason, 'stop')
+        self.assertTrue(choice.message.content, test_value)
+
+    def test_create_completion_with_2_choices(self):
+        test_value = "This is a test!"
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Say " + test_value,
+                }
+            ],
+            model=model,
+            n=2
+        )
+        self.assertTrue(len(chat_completion.choices), 2)
+
+    def test_create_embedding(self):
+        test_text = "This is a test!"
+        res = client.embeddings.create(input=test_text, model=model_with_embedding)
+        embedding = res.data[0].embedding
+        self.assertTrue(len(embedding) > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
