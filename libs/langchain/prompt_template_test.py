3c86c635b2025425823b8a95e41eaa356e43b22f
vduzh <vduzh@tut.by>
2024-02-13 15:34:52 +0300

    Tested PromptTemplate

--- libs/langchain/prompt_template_test.py
+++ libs/langchain/prompt_template_test.py
@@ -1,34 +1,33 @@
 import unittest
 
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
 
-from model import get_chat_model
+from model import get_llm
 
 
 class TestPromptTemplates(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Initialize the model
-        cls._llm = get_chat_model()
-
-        # convert the chat message to a string
+        cls._llm = get_llm()
         cls._output_parser = StrOutputParser()
 
     @classmethod
     def tearDownClass(cls):
         cls._llm = None
 
+    def test_create_prompt_from_template(self):
+        prompt = PromptTemplate.from_template("What is a good name for a company that makes colorful socks?")
+
+    def test_format_prompt(self):
+        prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
+        text = prompt.format(product="colorful socks")
+        self.assertEqual(text, "What is a good name for a company that makes colorful socks?")
+
     def test_basic_chain(self):
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are world class technical documentation writer."),
-            ("user", "{input}")
-        ])
-
+        prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
         chain = prompt | self._llm | self._output_parser
-
-        s = chain.invoke({"input": "how can langsmith help with testing?"})
-
+        out = chain.invoke({"product": "colorful socks"})
 
 if __name__ == '__main__':
     unittest.main()
