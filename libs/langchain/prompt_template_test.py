ee0d3c05147c85a444871a35d5979fb3f334e6db
vduzh <vduzh@tut.by>
2024-02-13 18:39:34 +0300

    Added new tests

--- libs/langchain/prompt_template_test.py
+++ libs/langchain/prompt_template_test.py
@@ -1,6 +1,8 @@
 import unittest
 
+from langchain_core.messages import HumanMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompt_values import StringPromptValue
 from langchain_core.prompts import PromptTemplate
 
 from model import get_llm
@@ -17,17 +19,41 @@ class TestPromptTemplates(unittest.TestCase):
         cls._llm = None
 
     def test_create_prompt_from_template(self):
-        prompt = PromptTemplate.from_template("What is a good name for a company that makes colorful socks?")
+        text = "What is a good name for a company that makes colorful socks?"
+        prompt_template = PromptTemplate.from_template(text)
+        self.assertEqual(prompt_template.template, text)
+
+    def test_create_prompt_with_string_prompt_composition(self):
+        template = "What is a good name for a company that makes colorful socks?"
+        text = ", make it funny and in {language}"
+        prompt_template = PromptTemplate.from_template(template) + text
+        self.assertEqual(prompt_template.template, template + text)
+        self.assertEqual(prompt_template.input_variables, ["language"])
 
     def test_format_prompt(self):
-        prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
-        text = prompt.format(product="colorful socks")
+        prompt_template = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
+        text = prompt_template.format(product="colorful socks")
         self.assertEqual(text, "What is a good name for a company that makes colorful socks?")
+
+    def test_invoke_string_prompt_value(self):
+        prompt_template = PromptTemplate.from_template("Test {foo}")
+
+        string_prompt_value = prompt_template.invoke({"foo": "Foo"})
+
+        self.assertIs(type(string_prompt_value), StringPromptValue)
+        self.assertEqual(string_prompt_value.to_string(), "Test Foo")
+
+        message_list = string_prompt_value.to_messages();
+        self.assertEqual(len(message_list), 1)
+        message = message_list[0]
+        self.assertIs(type(message), HumanMessage)
+        self.assertEqual(len(message_list), 1)
 
     def test_basic_chain(self):
         prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
-        chain = prompt | self._llm | self._output_parser
-        out = chain.invoke({"product": "colorful socks"})
+        # chain = prompt | self._llm | self._output_parser
+        # out = chain.invoke({"product": "colorful socks"})
+
 
 if __name__ == '__main__':
     unittest.main()
