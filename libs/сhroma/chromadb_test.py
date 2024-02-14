1daaf903d3c82b686077d03610c218ae0f721a65
vduzh <vduzh@tut.by>
2024-02-14 12:55:33 +0300

    Added Chroma

---
+++ libs/сhroma/chromadb_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestChromaDB(unittest.TestCase):
+
+    def test_create_embedding(self):
+        print("Working...")
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
