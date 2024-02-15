7178e39be66c5f0b4f95dff72ec1346fc4f8bcee
vduzh <vduzh@tut.by>
2024-02-15 16:52:16 +0300

    Added several tests

--- libs/langchain/llm_test.py
+++ libs/langchain/llm_test.py
@@ -13,15 +13,23 @@ class TestLLM(unittest.TestCase):
     def tearDownClass(cls):
         cls._llm = None
 
-    def test_input_str_and_output_str(self):
-        text = "This is a test!"
-        res = self._llm.invoke("Say " + text)
-        self.assertIs(type(res), str)
-
     def test_invoke(self):
-        text = "This is a test!"
-        res = self._llm.invoke("Say " + text)
-        self.assertEqual(res.strip(), text)
+        input_text = "This is a test!"
+        output_str = self._llm.invoke("Say " + input_text)
+        self.assertEqual(output_str.strip(), input_text)
+
+    def test_stream(self):
+        for chunk_str in self._llm.stream("What is a good name for a company that makes colorful socks?"):
+            print(chunk_str, end="", flush=True)
+
+    def test_batch(self):
+        messages = [
+            "What is a good name for a company that makes colorful socks?",
+            "What are some theories about the relationship between unemployment and inflation?",
+        ]
+
+        result_list = self._llm.batch(messages)
+        print(result_list)
 
 
 if __name__ == '__main__':
