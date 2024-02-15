82408ebedf337842d5307033a2ad5a655817d308
vduzh <vduzh@tut.by>
2024-02-15 15:33:12 +0300

    Tested new methods

--- libs/langchain/chat_model_test.py
+++ libs/langchain/chat_model_test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 from model import get_chat_model
 
 
-class TestLLM(unittest.TestCase):
+class TestChatModel(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Initialize the model
@@ -15,16 +15,82 @@ class TestLLM(unittest.TestCase):
     def tearDownClass(cls):
         cls.chat_model = None
 
+    def test_create_ai_message(self):
+        content = "The purpose of model regularization is to ..."
+        message = AIMessage(content=content)
+
+        self.assertEqual(message.type, "ai")
+        self.assertEqual(message.content, content)
+
+    def test_create_human_message(self):
+        content = "What is the purpose of model regularization?"
+        message = HumanMessage(content=content)
+
+        self.assertEqual(message.type, "human")
+        self.assertEqual(message.content, content)
+
+    def test_create_system_message(self):
+        content = "You're a helpful assistant"
+        message = SystemMessage(content=content)
+
+        self.assertEqual(message.type, "system")
+        self.assertEqual(message.content, content)
+
+    def test_create_function_message(self):
+        pass
+
+    def test_create_chat_message(self):
+        pass
+
     def test_input_messages_and_output_message(self):
         messages = [HumanMessage(content="This is a test!")]
         res = self.chat_model.invoke(messages)
         self.assertIs(type(res), AIMessage)
 
     def test_invoke(self):
-        text = "This is a test!"
-        messages = [HumanMessage(content="Say " + text)]
-        message = self.chat_model.invoke(messages)
-        self.assertEqual(message.content, text)
+        # list of messages
+        input_messages = [
+            SystemMessage(content="You're a helpful assistant"),
+            HumanMessage(content="What is the purpose of model regularization?"),
+        ]
+        out_message = self.chat_model.invoke(input_messages)
+        print(out_message)
+
+    # def test_invoke(self):
+    #     text = "This is a test!"
+    #     messages = [HumanMessage(content="Say " + text)]
+    #     message = self.chat_model.invoke(messages)
+    #     self.assertEqual(message.content, text)
+
+    def test_invoke_coerced_objects(self):
+        # TODO: implement
+        pass
+
+    def test_stream(self):
+        input_messages = [
+            SystemMessage(content="You're a helpful assistant"),
+            HumanMessage(content="What is the purpose of model regularization?"),
+        ]
+
+        for chunk in self.chat_model.stream(input_messages):
+            print(chunk.content, end="", flush=True)
+
+    def test_batch(self):
+        input_messages = [
+            SystemMessage(content="You're a helpful assistant"),
+            HumanMessage(content="What is the purpose of model regularization?"),
+        ]
+        out_message = self.chat_model.batch([input_messages])
+        print(out_message)
+
+    # unittest doesn't support async code
+    # async def test_a_invoke(self):
+    #     input_messages = [
+    #         SystemMessage(content="You're a helpful assistant"),
+    #         HumanMessage(content="What is the purpose of model regularization?"),
+    #     ]
+    #     out_message = await self.chat_model.ainvoke(input_messages)
+    #     print(out_message)
 
 
 if __name__ == '__main__':
