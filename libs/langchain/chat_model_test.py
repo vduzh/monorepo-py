699c652fc81f21787cad38c328545ba09597c981
vduzh <vduzh@tut.by>
2024-02-15 15:53:31 +0300

    Added a test for function calling

--- libs/langchain/chat_model_test.py
+++ libs/langchain/chat_model_test.py
@@ -1,8 +1,20 @@
+import json
 import unittest
 
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.utils.function_calling import convert_to_openai_tool
 
 from model import get_chat_model
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiply two integers together.
+
+    Args:
+        a: First integer
+        b: Second integer
+    """
+    return a * b
 
 
 class TestChatModel(unittest.TestCase):
@@ -91,6 +103,22 @@ class TestChatModel(unittest.TestCase):
     #     ]
     #     out_message = await self.chat_model.ainvoke(input_messages)
     #     print(out_message)
+
+    def test_python_function_calling(self):
+        # define function
+        tool = convert_to_openai_tool(multiply)
+        # print(json.dumps(tool, indent=2))
+
+        # out_message = self.chat_model.invoke(
+        #     "what's 5 times three",
+        #     tools=[convert_to_openai_tool(multiply)]
+        # )
+        # print(out_message)
+
+        # chat_model_with_tool = self.chat_model.bind(tools=[convert_to_openai_tool(multiply)])
+        # chat_model_with_tool.invoke("what's 5 times three")
+
+        pass
 
 
 if __name__ == '__main__':
