aec44da7a54fda427455c9a0c5c486ba7e4dafb9
vduzh <vduzh@tut.by>
2024-02-15 12:36:33 +0300

    Added the Partial prompt templates

--- libs/langchain/prompt_template_test.py
+++ libs/langchain/prompt_template_test.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from langchain_core.messages import HumanMessage
 from langchain_core.output_parsers import StrOutputParser
@@ -6,6 +7,10 @@ from langchain_core.prompt_values import StringPromptValue
 from langchain_core.prompts import PromptTemplate
 
 from model import get_llm
+
+
+def _get_foo():
+    return "Foo"
 
 
 class TestPromptTemplates(unittest.TestCase):
@@ -18,22 +23,38 @@ class TestPromptTemplates(unittest.TestCase):
     def tearDownClass(cls):
         cls._llm = None
 
-    def test_create_prompt_from_template(self):
-        text = "What is a good name for a company that makes colorful socks?"
-        prompt_template = PromptTemplate.from_template(text)
-        self.assertEqual(prompt_template.template, text)
+    def test_create_prompt_template(self):
+        template = "What is a good name for a company that makes {product}?"
+        prompt_template = PromptTemplate(
+            template=template,
+            input_variables=["product"]
+        )
+        # print(prompt_template)
+
+    def test_create_prompt_template_from_template(self):
+        template = "What is a good name for a company that makes {product}?"
+        prompt_template = PromptTemplate.from_template(template)
+        print(prompt_template)
+
+        self.assertEqual(prompt_template.template, template)
+        self.assertEqual(prompt_template.input_variables, ["product"])
 
     def test_create_prompt_with_string_prompt_composition(self):
         template = "What is a good name for a company that makes colorful socks?"
         text = ", make it funny and in {language}"
         prompt_template = PromptTemplate.from_template(template) + text
+
         self.assertEqual(prompt_template.template, template + text)
         self.assertEqual(prompt_template.input_variables, ["language"])
 
     def test_format_prompt(self):
-        prompt_template = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
-        text = prompt_template.format(product="colorful socks")
-        self.assertEqual(text, "What is a good name for a company that makes colorful socks?")
+        template = "What is a good name for a company that makes {product}?"
+        prompt_template = PromptTemplate.from_template(template)
+
+        product = "colorful socks"
+        prompt_text = prompt_template.format(product=product)
+
+        self.assertEqual(prompt_text, template.replace("{product}", product))
 
     def test_invoke_string_prompt_value(self):
         prompt_template = PromptTemplate.from_template("Test {foo}")
@@ -48,6 +69,22 @@ class TestPromptTemplates(unittest.TestCase):
         message = message_list[0]
         self.assertIs(type(message), HumanMessage)
         self.assertEqual(len(message_list), 1)
+
+    def test_partial_prompt_templates_with_strings(self):
+        prompt_template = PromptTemplate.from_template("{foo}{bar}")
+        partial_prompt_template = prompt_template.partial(foo="Foo")
+
+        template_text = partial_prompt_template.format(bar="Bar")
+
+        self.assertEqual(template_text, "FooBar")
+
+    def test_partial_prompt_templates_with_functions(self):
+        prompt_template = PromptTemplate.from_template("{foo}{bar}")
+        partial_prompt_template = prompt_template.partial(foo=_get_foo)
+
+        template_text = partial_prompt_template.format(bar="Bar")
+
+        self.assertEqual(template_text, "FooBar")
 
     def test_basic_chain(self):
         prompt = PromptTemplate.from_template("What is a good name for a company that makes {product}?")
