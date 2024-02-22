03875bbb02c5c1b3998eaab6d4669962c3189134
vduzh <vduzh@tut.by>
2024-02-22 18:01:09 +0300

    Added new tests

--- libs/langchain/agents_test.py
+++ libs/langchain/agents_test.py
@@ -1,12 +1,17 @@
 import unittest
+from pprint import pprint
 
 from dotenv import load_dotenv
-from langchain.agents import tool, AgentExecutor
+from langchain import hub
+from langchain.agents import tool, AgentExecutor, create_openai_functions_agent, create_openai_tools_agent, \
+    create_json_chat_agent, create_structured_chat_agent
 from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 from langchain.agents.format_scratchpad import format_to_openai_function_messages
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.agents import AgentFinish
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.utils.function_calling import format_tool_to_openai_function
 
@@ -15,29 +20,30 @@ from libs.langchain.model import get_chat_model
 load_dotenv()
 
 
-# function name is also used by LLM to find a function
 @tool
 def get_word_length(word: str) -> int:
-    """Returns the length of a word."""
-    # """use this tool when you need to calculate the length of a car using its name."""
+    """use this tool when you need to calculate the length of a word."""
     return len(word)
 
 
-# function name is also used by LLM to find a function
 @tool
 def get_product_calories(product: str) -> int:
-    """Calculates the amount of calories of the product."""
-    # """use this tool when you need to calculate the number of calories the products contains."""
-    # """use this tool when you need to calculate the price of the product."""
+    """use this tool when you need to calculate the number of calories the products contains."""
     return len(product * 100)
 
 
-class TestTemplate(unittest.TestCase):
+# class CaloriesCalculatorTool(BaseTool):
+#     name = "Calories calculator"
+#     description = "use this tool when you need to calculate the amount of calories a product contains."
+#
+#     def _run(self, product: str) -> int:
+#         return len(product * 100)
+#
+#     def _arun(self, product: str) -> int:
+#         raise NotImplementedError("This tool does not support async")
 
-    def test_duck_duck_go_tool(self):
-        search_tool = DuckDuckGoSearchRun()
-        result = search_tool.run("Manchester United vs Luton town match summary")
-        # print("DuckDuckGoSearchRun:result", result)
+
+class TestAgents(unittest.TestCase):
 
     def test_simple_agent(self):
         tools = [DuckDuckGoSearchRun()]
@@ -47,7 +53,7 @@ class TestTemplate(unittest.TestCase):
             verbose=True)
 
         result = agent_executor({"input": "Manchester United vs Luton town match summary"})
-        # print("simple_agent:", result["output"])
+        print("simple_agent:", result["output"])
 
     def test_build_agent_from_scratch_with_custom_executor(self):
         # create the prompt for the agent
@@ -57,6 +63,7 @@ class TestTemplate(unittest.TestCase):
                     "system",
                     "You are a very powerful assistant but not great at calculating word lengths.",
                 ),
+                MessagesPlaceholder(variable_name="chat_history"),
                 ("user", "{input}"),
                 MessagesPlaceholder(variable_name="agent_scratchpad"),
             ]
@@ -77,30 +84,35 @@ class TestTemplate(unittest.TestCase):
                 {
                     "input": lambda x: x["input"],
                     "agent_scratchpad": lambda x: format_to_openai_function_messages(x["intermediate_steps"]),
+                    "chat_history": lambda х: х["chat_history"],
                 }
                 | prompt
                 | llm_with_tools
                 | OpenAIFunctionsAgentOutputParser()
         )
 
-        # write a runtime for the agent
+        # write a runtime (agent executor) for the agent
         user_inputs = [
             "how many letters in the word educa?",
             "How fatty is a croissant? Return the result as a number."
         ]
         for user_input in user_inputs:
+            chat_history = []
             intermediate_steps = []
             while True:
                 # the next action to take
                 agent_action = agent.invoke({
                     "input": user_input,
-                    "intermediate_steps": intermediate_steps
+                    "intermediate_steps": intermediate_steps,
+                    "chat_history": chat_history
                 })
                 print("agent_action:", type(agent_action))
 
                 if isinstance(agent_action, AgentFinish):
-                    final_resul = agent_action.return_values["output"]
-                    print("final_result:", final_resul)
+                    final_result_str = agent_action.return_values["output"]
+                    print("final_result:", final_result_str)
+                    chat_history.extend([HumanMessage(content=user_input), AIMessage(content=final_result_str)])
+                    print("chat_history:", chat_history)
                     break
                 else:
                     print(f"TOOL NAME: {agent_action.tool}")
@@ -118,6 +130,7 @@ class TestTemplate(unittest.TestCase):
                     "system",
                     "You are a very powerful assistant but not great at calculating word lengths.",
                 ),
+                MessagesPlaceholder(variable_name="chat_history"),
                 ("user", "{input}"),
                 MessagesPlaceholder(variable_name="agent_scratchpad"),
             ]
@@ -130,6 +143,7 @@ class TestTemplate(unittest.TestCase):
                 {
                     "input": lambda x: x["input"],
                     "agent_scratchpad": lambda x: format_to_openai_function_messages(x["intermediate_steps"]),
+                    "chat_history": lambda х: х["chat_history"],
                 }
                 | prompt
                 | llm_with_tools
@@ -140,38 +154,127 @@ class TestTemplate(unittest.TestCase):
             "How many letters in the word educa? Return the result as a number.",
             "How fatty is a croissant? Return the result as a number."
         ]
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
         for user_input in user_inputs:
-            agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-            out_dict = agent_executor.invoke({"input": user_input})
-            print("res:", out_dict["output"])
+            chat_history = []
+            out_dict = agent_executor.invoke({"input": user_input, "chat_history": chat_history})
+            result_str = out_dict["output"]
+            print("result:", result_str)
+            chat_history.extend([HumanMessage(content=user_input), AIMessage(content=result_str)])
+            print("chat_history:", chat_history)
+
+    def test_openai_functions_agent_deprecated(self):
+        # initialize tools
+        tools = [TavilySearchResults(max_results=1)]
+
+        # Create Agent
+        # get the prompt to use - you can modify this!
+        prompt = hub.pull("hwchase17/openai-functions-agent")
+        # get LLM that will drive the agent
+        llm = get_chat_model()
+        # construct the OpenAI Functions agent
+        agent = create_openai_functions_agent(llm, tools, prompt)
+
+        # Run Agent
+        # create an agent executor by passing in the agent and tools
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+        res_str = agent_executor.invoke({"input": "what is LangChain?"})
+        pprint(res_str["output"])
+
+        # Run Agent with chat history
+        res_str = agent_executor.invoke(
+            {
+                "input": "what's my name?",
+                "chat_history": [
+                    HumanMessage(content="hi! my name is bob"),
+                    AIMessage(content="Hello Bob! How can I assist you today?"),
+                ],
+            }
+        )
+        pprint(res_str["output"])
+
+    def test_openai_tools_agent(self):
+        # initialize tools
+        tools = [TavilySearchResults(max_results=1)]
+
+        # create agent
+        prompt = hub.pull("hwchase17/openai-tools-agent")
+        llm = get_chat_model()
+        agent = create_openai_tools_agent(llm, tools, prompt)
+
+        # run agent
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+        res_str = agent_executor.invoke({"input": "what is LangChain?"})
+        pprint(res_str["output"])
+
+        # Run Agent with chat history
+        res_str = agent_executor.invoke(
+            {
+                "input": "what's my name? Don't use tools to look this up unless you NEED to",
+                "chat_history": [
+                    HumanMessage(content="hi! my name is bob"),
+                    AIMessage(content="Hello Bob! How can I assist you today?"),
+                ],
+            }
+        )
+        pprint(res_str["output"])
+
+    def test_json_chat_agent(self):
+        # initialize tools
+        tools = [TavilySearchResults(max_results=1)]
+
+        # create agent
+        prompt = hub.pull("hwchase17/react-chat-json")
+        llm = get_chat_model()
+        agent = create_json_chat_agent(llm, tools, prompt)
+
+        # run agent
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+
+        res_str = agent_executor.invoke({"input": "what is LangChain?"})
+        pprint(res_str["output"])
+
+        # Run Agent with chat history
+        res_str = agent_executor.invoke(
+            {
+                "input": "what's my name?",
+                "chat_history": [
+                    HumanMessage(content="hi! my name is bob"),
+                    AIMessage(content="Hello Bob! How can I assist you today?"),
+                ],
+            }
+        )
+        pprint(res_str["output"])
+
+    def test_structured_chat_agent(self):
+        # initialize tools
+        tools = [TavilySearchResults(max_results=1)]
+
+        # create agent
+        prompt = hub.pull("hwchase17/structured-chat-agent")
+        llm = get_chat_model()
+        agent = create_structured_chat_agent(llm, tools, prompt)
+
+        # run agent
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+
+        res_str = agent_executor.invoke({"input": "what is LangChain?"})
+        pprint(res_str["output"])
+
+        # Run Agent with chat history
+        res_str = agent_executor.invoke(
+            {
+                "input": "what's my name? Do not use tools unless you have to",
+                "chat_history": [
+                    HumanMessage(content="hi! my name is bob"),
+                    AIMessage(content="Hello Bob! How can I assist you today?"),
+                ],
+            })
+        pprint(res_str["output"])
 
     #
-    # def test_shell_tool(self):
-    #     shell_tool = ShellTool()
-    #     print(shell_tool.description)
-    #
-    #     result = shell_tool.run({"commands": ["echo 'Hello World!'", "time"]})
-    #     # print(result)
-
-    # def test_google_search(self):
-    #     search = GoogleSearchAPIWrapper()
-    #
-    #     search_tool = Tool(
-    #         name="Intermediate Answer",
-    #         description="Search google for recent results",
-    #         func=search.run
-    #     )
-    #
-    #     google_serach_agent = initialize_agent(
-    #         agent="self-ask-with-search",
-    #         tools=[search_tool],
-    #         llm=get_chat_model(),
-    #         verbose=True,
-    #         max_iterations=10
-    #     )
-    #
-    #     res = google_serach_agent.run("The news about langchain")
-    #     print(res)
 
     # def test_retrival_agent(self):
     #     # create a retriever over some data of our own.
@@ -230,6 +333,26 @@ class TestTemplate(unittest.TestCase):
     #     # Get the prompt to use - you can modify this!
     #     prompt = hub.pull("hwchase17/openai-functions-agent")
     #     print(prompt.messages)
+
+    # def test_google_search(self):
+    #     search = GoogleSearchAPIWrapper()
+    #
+    #     search_tool = Tool(
+    #         name="Intermediate Answer",
+    #         description="Search google for recent results",
+    #         func=search.run
+    #     )
+    #
+    #     google_serach_agent = initialize_agent(
+    #         agent="self-ask-with-search",
+    #         tools=[search_tool],
+    #         llm=get_chat_model(),
+    #         verbose=True,
+    #         max_iterations=10
+    #     )
+    #
+    #     res = google_serach_agent.run("The news about langchain")
+    #     print(res)
 
 
 if __name__ == '__main__':
---
+++ libs/langchain/tools_test.py
@@ -0,0 +1,291 @@
+import unittest
+from pprint import pprint
+from typing import Type, Optional
+
+from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
+from langchain_community.tools.file_management import MoveFileTool
+from langchain_community.tools.shell import ShellTool
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from langchain_core.callbacks import CallbackManagerForToolRun, AsyncCallbackManagerForToolRun
+from langchain_core.messages import HumanMessage
+from langchain_core.tools import BaseTool, StructuredTool
+from langchain_core.utils.function_calling import convert_to_openai_function
+from pydantic.v1 import BaseModel, Field
+
+from libs.langchain.model import get_chat_model
+
+load_dotenv()
+
+
+class WikiInputs(BaseModel):
+    """Inputs to the wikipedia tool."""
+
+    query: str = Field(
+        description="query to look up in Wikipedia, should be 3 or less words"
+    )
+
+
+@tool
+def search(query: str) -> str:
+    """Look up things online."""
+    return "LangChain"
+
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers."""
+    return a * b
+
+
+class WordInput(BaseModel):
+    name: str = Field(description="should be a word")
+
+
+# function name is also used by LLM to find a function
+@tool("get_word_length", args_schema=WordInput, return_direct=True)
+def get_word_length(word: str) -> int:
+    """Returns the length of a word."""
+    # """use this tool when you need to calculate the length of a car using its name."""
+    return len(word)
+
+
+# function name is also used by LLM to find a function
+# @tool
+# def get_product_calories(product: str) -> int:
+#     """Calculates the amount of calories of the product."""
+#     # """use this tool when you need to calculate the number of calories the products contains."""
+#     # """use this tool when you need to calculate the price of the product."""
+#     return len(product * 100)
+
+
+class ProductInput(BaseModel):
+    name: str = Field(description="should be the name of the product")
+
+
+class CaloriesCalculatorTool(BaseTool):
+    name = "calories_calculator"
+    description = "use this tool when you need to calculate the amount of calories a product contains."
+    args_schema: Type[BaseModel] = ProductInput
+
+    def _run(self, name: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> int:
+        """Use the tool."""
+        return len(name * 100)
+
+    def _arun(self, name: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> int:
+        raise NotImplementedError("calories_calculator tool does not support async")
+
+
+def say_hello(name: str) -> str:
+    """Greets a person."""
+    return f"Hello {name}!"
+
+
+class TestTools(unittest.TestCase):
+    def test_built_in_tool(self):
+        api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+        search_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+        # wikipedia
+        print("default name:", search_tool.name)
+
+        # A wrapper around Wikipedia. Useful for when you need to answer general questions about people, places,
+        # companies, facts, historical events, or other subjects. Input should be a search query.
+        print("default description:", search_tool.description)
+
+        # {'query': {'title': 'Query', 'type': 'string'}}
+        print("default JSON schema of the inputs:", search_tool.args)
+
+        # if the tool should return directly to the user
+        print("return directly to the user:", search_tool.return_direct)
+
+        # Call the tool with a dictionary input
+        res_str = search_tool.run({"query": "langchain"})
+        print("result of call #1:", res_str)
+        # Call the tool with a single string input
+        res_str = search_tool.run("langchain")
+        print("result of call #2:", res_str)
+
+    def test_customizing_built_in_tool(self):
+        api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+        search_tool = WikipediaQueryRun(
+            name="wiki-tool",
+            description="look up things in wikipedia",
+            args_schema=WikiInputs,
+            api_wrapper=api_wrapper,
+            return_direct=True,
+        )
+
+        print("default name:", search_tool.name)
+        print("default description:", search_tool.description)
+        print("default JSON schema of the inputs:", search_tool.args)
+        print("return directly to the user:", search_tool.return_direct)
+
+        res_str = search_tool.run("langchain")
+        print("result of call:", res_str)
+
+    def test_built_in_shell_tool(self):
+        shell_tool = ShellTool()
+        print(shell_tool.description)
+
+        result = shell_tool.run({"commands": ["echo 'Hello World!'", "time"]})
+        pprint(result)
+
+    def test_built_in_duck_duck_go_tool(self):
+        search_tool = DuckDuckGoSearchRun()
+        result = search_tool.run("Manchester United vs Luton town match summary")
+        # print("DuckDuckGoSearchRun:result", result)
+
+    def test_custom_tool_with_tool_decorator(self):
+        custom_tool = search
+
+        print("default name:", custom_tool.name)
+        print("default description:", custom_tool.description)
+        print("default JSON schema of the inputs:", custom_tool.args)
+        print("return directly to the user:", custom_tool.return_direct)
+
+    def test_custom_tool_with_multiple_inputs(self):
+        custom_tool = multiply
+
+        print("default name:", custom_tool.name)
+        print("default description:", custom_tool.description)
+        print("default JSON schema of the inputs:", custom_tool.args)
+        print("return directly to the user:", custom_tool.return_direct)
+
+    def test_custom_tool_with_subclassing_base_tool(self):
+        custom_tool = CaloriesCalculatorTool()
+
+        print("default name:", custom_tool.name)
+        print("default description:", custom_tool.description)
+        print("default JSON schema of the inputs:", custom_tool.args)
+        print("return directly to the user:", custom_tool.return_direct)
+
+        res_str = custom_tool.run({"name": "lemon"})
+        self.assertEqual(500, res_str)
+
+        res_str = custom_tool.run("orange")
+        self.assertEqual(600, res_str)
+
+    def test_custom_tool_with_structured_tool_dataclass(self):
+        custom_tool = StructuredTool.from_function(
+            func=say_hello,
+            name="Greeting",
+            description="useful for when you need to somebody with its name",
+        )
+
+        print("default name:", custom_tool.name)
+        print("default description:", custom_tool.description)
+        print("default JSON schema of the inputs:", custom_tool.args)
+        print("return directly to the user:", custom_tool.return_direct)
+
+        res_str = custom_tool.run("John")
+        self.assertEqual("Hello John!", res_str)
+
+    def test_tools_as_openai_functions(self):
+        tools = [MoveFileTool()]
+        functions = [convert_to_openai_function(t) for t in tools]
+
+        model = get_chat_model()
+        ai_message = model.invoke([HumanMessage(content="move file foo to bar")], functions=functions)
+        # pprint(ai_message)
+
+    def test_tools_as_openai_functions_with_auto_bind(self):
+        tools = [MoveFileTool()]
+        model_with_functions = get_chat_model().bind_functions(tools)
+
+        ai_message = model_with_functions.invoke([HumanMessage(content="move file foo to bar")])
+        # pprint(ai_message)
+
+    def test_tools_as_openai_functions_with_tool_choice(self):
+        tools = [MoveFileTool()]
+        model_with_tools = get_chat_model().bind_tools(tools)
+
+        ai_message = model_with_tools.invoke([HumanMessage(content="move file foo to bar")])
+        pprint(ai_message)
+
+    def test_toolkit(self):
+        raise NotImplementedError("test_toolkit is not implemented yet!")
+
+    # def test_google_search(self):
+    #     search = GoogleSearchAPIWrapper()
+    #
+    #     search_tool = Tool(
+    #         name="Intermediate Answer",
+    #         description="Search google for recent results",
+    #         func=search.run
+    #     )
+    #
+    #     google_serach_agent = initialize_agent(
+    #         agent="self-ask-with-search",
+    #         tools=[search_tool],
+    #         llm=get_chat_model(),
+    #         verbose=True,
+    #         max_iterations=10
+    #     )
+    #
+    #     res = google_serach_agent.run("The news about langchain")
+    #     print(res)
+
+    # def test_retrival_agent(self):
+    #     # create a retriever over some data of our own.
+    #     loader = WebBaseLoader("https://docs.smith.langchain.com/")
+    #     docs = loader.load()
+    #     vector = FAISS.from_documents(
+    #         RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(docs),
+    #         get_embeddings()
+    #     )
+    #     retriever = vector.as_retriever()
+    #
+    #     retriever_tool = create_retriever_tool(
+    #         retriever,
+    #         "langsmith_search",
+    #         "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
+    #     )
+    #
+    #     # create a list of tools
+    #     tools = [retriever_tool]
+    #
+    #     # create agent
+    #     agent_executor = create_conversational_retrieval_agent(get_chat_model(), tools, verbose=True)
+    #
+    #     q = "What is LangSmith?"
+    #     result = agent_executor({"input": q})
+    #     print("retrival_agent:", result["output"])
+
+    # def test_define_tools(self):
+    #     # create search tool
+    #     search_tool = TavilySearchResults()
+    #     # res = search_tool.invoke("what is the weather in SF")
+    #
+    #     # create a retriever over some data of our own.
+    #     loader = WebBaseLoader("https://docs.smith.langchain.com/")
+    #     docs = loader.load()
+    #     vector = FAISS.from_documents(
+    #         RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(docs),
+    #         get_embeddings()
+    #     )
+    #     retriever = vector.as_retriever()
+    #     # res = retriever.get_relevant_documents("how to upload a dataset")[0]
+    #
+    #     retriever_tool = create_retriever_tool(
+    #         retriever,
+    #         "langsmith_search",
+    #         "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
+    #     )
+    #     # print(retriever_tool)
+    #
+    #     # create a list of tools
+    #     tools = [search_tool, retriever_tool]
+    #
+    #     # create agent
+    #     llm = get_chat_model()
+    #
+    #     # Get the prompt to use - you can modify this!
+    #     prompt = hub.pull("hwchase17/openai-functions-agent")
+    #     print(prompt.messages)
+
+
+if __name__ == '__main__':
+    unittest.main()
