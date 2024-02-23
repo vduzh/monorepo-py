f448e90d616caecb91bc6150bbb749440ad899e1
vduzh <vduzh@tut.by>
2024-02-23 16:23:33 +0300

    some code added

--- libs/langchain/rag_test.py_test.py
+++ libs/langchain/rag_test.py_test.py
@@ -7,12 +7,31 @@ from langchain_openai import OpenAIEmbeddings
 class TestRetrievalAugmentedGeneration(unittest.TestCase):
 
     def test_(self):
-        vectorstore = DocArrayInMemorySearch.from_texts(
-            ["harrison worked at kensho", "bears like to eat honey"],
-            embedding=OpenAIEmbeddings(),
-        )
+        # https://python.langchain.com/docs/use_cases/question_answering/
 
-        retriever = vectorstore.as_retriever()
+        # vectorstore = Chroma.from_documents(
+        #     get_documents_from_file("state_of_the_union.txt"),
+        #     get_embeddings()
+        # )
+        # retriever = vectorstore.as_retriever()
+        #
+        # prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+        #
+        # combine_docs_chain = create_stuff_documents_chain(get_llm(), prompt)
+        #
+        # retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
+        #
+        # out_dict = retrieval_chain.invoke(
+        #     {
+        #         "input": "What did the president say about Ketanji Brown Jackson?",
+        #         "context": [],
+        #         "chat_history": [],
+        #     },
+        #     # config={'callbacks': [ConsoleCallbackHandler()]}
+        # )
+        # # pprint(out_dict)
+        # print(out_dict["answer"])
+        pass
 
 
 if __name__ == '__main__':
