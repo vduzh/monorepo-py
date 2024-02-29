d754500b2b9849ff8aba8161ddcdf820b1099424
vduzh <vduzh@tut.by>
2024-02-29 18:19:05 +0300

    added llm interaction

--- apps/chatbot/main.py
+++ apps/chatbot/main.py
@@ -1,62 +1,33 @@
-# import argparse
-
 from dotenv import load_dotenv
 
-# from langchain.chains import LLMChain, SequentialChain
-# from langchain_core.prompts import PromptTemplate
-# from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env file
 load_dotenv()
 
 
 def main():
+    llm = ChatOpenAI()
+
+    # system_template = "You are a chatbot specializing {subject}."
+    human_template = "{content}"
+    chat_prompt_template = ChatPromptTemplate.from_messages(
+        [
+            # ("system", system_template),
+            ("human", human_template)
+        ]
+    )
+
+    chain = chat_prompt_template | llm
+
     while True:
-        content = input(">> ")
+        content = input("User: ")
         if content == "exit":
             break
 
-        print(f"You entered: {content}")
-    pass
-
-    # extract app arguments from the command line
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--language", default="python")
-    # parser.add_argument("--task", default="return a list of numbers")
-    #
-    # args = parser.parse_args()
-    #
-    # language = args.language
-    # task = args.task
-    #
-    # print("== INPUT ==")
-    # print("Language:", language)
-    # print("Task:", task, end="\n\n")
-
-    # build the chain
-    # llm = ChatOpenAI()
-    #
-    # code_chain = LLMChain(
-    #     llm=llm,
-    #     prompt=PromptTemplate.from_template("Write a {language} function that will {task}."),
-    #     output_key="code"
-    # )
-    #
-    # test_chain = LLMChain(
-    #     llm=llm,
-    #     prompt=PromptTemplate.from_template("Write a unit-test for the following {language} code: {code}"),
-    #     output_key="test"
-    # )
-    #
-    # chain = SequentialChain(
-    #     chains=[code_chain, test_chain],
-    #     input_variables=["language", "task"],
-    #     output_variables=["code", "test"],
-    # )
-    #
-    # out = chain.invoke({"language": language, "task": task})
-    # print(">>>>>>>> GENERATED CODE:", out["code"], sep="\n", end="\n\n")
-    # print(">>>>>>>> GENERATED TEST:", out["test"], sep="\n", end="\n\n")
+        message = chain.invoke({"content": content})
+        print("AI:", message.content)
 
 
 if __name__ == "__main__":
