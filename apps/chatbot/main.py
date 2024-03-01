809a3a8d3ec1d9d8c0635a2645baff25a65f1449
vduzh <vduzh@tut.by>
2024-03-01 13:00:23 +0300

    Applied memories

--- apps/chatbot/main.py
+++ apps/chatbot/main.py
@@ -1,6 +1,11 @@
-from dotenv import load_dotenv
+from operator import itemgetter
+from pprint import pprint
 
-from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, HumanMessagePromptTemplate
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import ChatOpenAI
 
 # Load environment variables from .env file
@@ -10,24 +15,38 @@ load_dotenv()
 def main():
     llm = ChatOpenAI()
 
+    memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
     # system_template = "You are a chatbot specializing {subject}."
+    system_template = "You are a helpful assistant."
     human_template = "{content}"
     chat_prompt_template = ChatPromptTemplate.from_messages(
         [
-            # ("system", system_template),
-            ("human", human_template)
+            ("system", system_template),
+            MessagesPlaceholder(variable_name="messages"),
+            # ("human", human_template)
+            HumanMessagePromptTemplate.from_template(human_template)
         ]
     )
 
-    chain = chat_prompt_template | llm
+    chain = (
+        RunnablePassthrough.assign(
+            messages=RunnableLambda(memory.load_memory_variables) | itemgetter("messages")
+        )
+        | chat_prompt_template
+        | llm
+    )
 
     while True:
         content = input("User: ")
         if content == "exit":
             break
 
-        message = chain.invoke({"content": content})
+        inputs = {"content": content}
+        message = chain.invoke(inputs)
+
         print("AI:", message.content)
+        memory.save_context(inputs, {"output": message.content})
 
 
 if __name__ == "__main__":
