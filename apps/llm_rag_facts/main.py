b93bc5187ad03c5e8c7b8fa72e3a0fb1f81432fc
vduzh <vduzh@tut.by>
2024-03-04 18:33:41 +0300

    Applied retriever

--- apps/llm_rag_facts/main.py
+++ apps/llm_rag_facts/main.py
@@ -1,16 +1,31 @@
+from pprint import pprint
+
 from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
 from langchain_community.vectorstores.chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
 # Load environment variables from .env file
 load_dotenv()
 
 
 def main():
+    chat = ChatOpenAI()
+
     db = Chroma(
         persist_directory="./tmp/emb",
         embedding_function=OpenAIEmbeddings()
     )
+    retriever = db.as_retriever()
+
+    chain = RetrievalQA.from_chain_type(
+        llm=chat,
+        retriever=retriever,
+        chain_type="stuff"
+    )
+
+    res = chain.invoke({"query": "What is an interesting fact about the English language?"})
+    pprint(res)
 
     # TODO: update unit tests!!!
     # results = db.similarity_search_with_score(
@@ -24,12 +39,12 @@ def main():
     #     score = result[1]
     #     print(score, doc.page_content, sep="\n", end="\n\n")
 
-    results = db.similarity_search(
-        "What is an interesting fat about the English language?",
-        k=1
-    )
-    for result in results:
-        print(result.page_content)
+    # results = db.similarity_search(
+    #     "What is an interesting fat about the English language?",
+    #     k=1
+    # )
+    # for result in results:
+    #     print(result.page_content)
 
 
 if __name__ == "__main__":
