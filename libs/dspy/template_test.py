2d09cd99add538a06774d91a85f1c688edba5d77
vduzh <vduzh@tut.by>
2024-04-18 11:43:04 +0300

    added setup and teardown methods

--- libs/dspy/template_test.py
+++ libs/dspy/template_test.py
@@ -3,6 +3,19 @@ from pprint import pprint
 
 
 class TestTemplate(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._foo = "foo"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._foo = None
+
+    def setUp(self):
+        self._bar = "bar"
+
+    def tearDown(self):
+        self._bar = None
 
     def test_foo(self):
         pprint("Testing foo")
