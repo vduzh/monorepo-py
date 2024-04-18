263424440eede06ce31b408b24f3920554ee590c
vduzh <vduzh@tut.by>
2024-04-18 17:05:56 +0300

    added test_inline_signatures_qa

--- libs/dspy/signature_test.py
+++ libs/dspy/signature_test.py
@@ -5,7 +5,12 @@ import dspy
 
 from model import get_lm
 
+QUESTION = "What is the capital of Germany?"
 
+
+# A signature is a declarative specification of input/output behavior of a DSPy module
+#
+# Signatures tell the LM what it needs to do, rather than specify how we should ask the LM to do it.
 class TestSignature(TestCase):
 
     @classmethod
@@ -15,6 +20,15 @@ class TestSignature(TestCase):
         lm.inspect_history(n=1)
         dspy.settings.configure(lm=lm)
 
+    def test_inline_signatures_qa(self):
+        signature = "question -> answer"
+        qa = dspy.Predict(signature)
+
+        response = qa(question=QUESTION)
+        print(response)
+
+        self.assertEqual("Berlin", response.answer)
+
     def test_lm(self):
         # Define the signature (return an answer, given a question)
         signature_str = "question -> answer"
@@ -22,7 +36,7 @@ class TestSignature(TestCase):
         qa_module = dspy.ChainOfThought(signature_str)
 
         # Run with the default LM configured with `dspy.configure` above.
-        response_as_prediction = qa_module(question="What is the capital of Germany?")
+        response_as_prediction = qa_module(question=QUESTION)
         print(response_as_prediction)
 
         self.assertEqual("Berlin", response_as_prediction.answer)
