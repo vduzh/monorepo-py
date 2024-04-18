27da80e137cea9f522d07e3747ab874b1e98ef2a
vduzh <vduzh@tut.by>
2024-04-18 16:49:29 +0300

    added placeholders for several tests

--- libs/dspy/lm_test.py
+++ libs/dspy/lm_test.py
@@ -33,15 +33,43 @@ class TestLanguageModels(TestCase):
         # Run with the default model
         qa_module = dspy.ChainOfThought('question -> answer')
         res = qa_module(question=self.__QUESTION)
-        print("Default LM:",  res)
+        print("Default LM:", res)
         self.assertEqual("Berlin", res.answer)
 
         # Run with GPT-4
         another_lm = dspy.OpenAI(model='gpt-4-1106-preview', max_tokens=300)
         with dspy.context(lm=another_lm):
             res = qa_module(question=self.__QUESTION)
-            print("Another LM:",  res)
+            print("Another LM:", res)
             self.assertEqual("Berlin", res.answer)
+
+    def test_remote_lm_open_ai(self):
+        # lm = dspy.{provider_listed_below}(model="your model", model_request_kwargs="...")
+        lm = dspy.OpenAI(model="your model", model_request_kwargs="...")
+        raise NotImplementedError("Not implemented yet")
+
+    def test_remote_lm_cohere(self):
+        # lm = dspy.{provider_listed_below}(model="your model", model_request_kwargs="...")
+        lm = dspy.Cohere(model="your model", model_request_kwargs="...")
+        raise NotImplementedError("Not implemented yet")
+
+    def test_remote_lm_anyscale(self):
+        # lm = dspy.{provider_listed_below}(model="your model", model_request_kwargs="...")
+        lm = dspy.Anyscale(model="your model", model_request_kwargs="...")
+        raise NotImplementedError("Not implemented yet")
+
+    def test_remote_lm_together(self):
+        # lm = dspy.{provider_listed_below}(model="your model", model_request_kwargs="...")
+        lm = dspy.Together(model="your model", model_request_kwargs="...")
+        raise NotImplementedError("Not implemented yet")
+
+    def test_local_lm_hf_client_tgi(self):
+        # tgi_mistral = dspy.HFClientTGI(
+        #     model="mistralai/Mistral-7B-Instruct-v0.2",
+        #     port=8080,
+        #     url="http://localhost"
+        # )
+        raise NotImplementedError("Not implemented yet")
 
 
 if __name__ == '__main__':
