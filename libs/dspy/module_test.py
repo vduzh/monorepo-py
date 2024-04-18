9e41ebef68e2eb2e50ea760238b7a627b984a500
vduzh <vduzh@tut.by>
2024-04-18 17:20:23 +0300

    added test_predict

--- libs/dspy/module_test.py
+++ libs/dspy/module_test.py
@@ -5,9 +5,10 @@ import dspy
 
 from model import get_lm
 
+QUESTION = "What is the capital of Germany?"
+
 
 class TestModule(TestCase):
-    __QUESTION = "What is the capital of Germany?"
 
     @classmethod
     def setUpClass(cls):
@@ -15,12 +16,25 @@ class TestModule(TestCase):
         # lm.inspect_history(n=1)
         dspy.settings.configure(lm=lm)
 
+    def test_predict(self):
+        predict_module = dspy.Predict("question -> answer")
+
+        res = predict_module(question=QUESTION)
+        print(res)
+
+        # there is no auxiliary information just
+        self.assertEqual(1, len(res))
+        self.assertEqual("Berlin", res.answer)
+
     def test_chain_of_thought(self):
         qa_module = dspy.ChainOfThought("question -> answer")
 
-        res = qa_module(question=self.__QUESTION)
+        res = qa_module(question=QUESTION)
         print(res)
-        self.assertTrue(res.answer.find("Berlin") > -1)
+
+        self.assertEqual(2, len(res))
+        self.assertIsNotNone(res.rationale)
+        self.assertEqual("Berlin", res.answer)
 
     def test_chain_of_thought_num_of_outputs(self):
         qa_module = dspy.ChainOfThought("question -> answer", n=5)
