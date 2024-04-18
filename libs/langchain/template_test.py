5f9b5d899d2ca5fd13497b8a301ff7d1d774efd8
vduzh <vduzh@tut.by>
2024-04-18 11:43:27 +0300

    added setup and teardown methods

--- libs/langchain/template_test.py
+++ libs/langchain/template_test.py
@@ -3,12 +3,23 @@ from pprint import pprint
 
 
 class TestTemplate(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._foo = "foo"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._foo = None
+
+    def setUp(self):
+        self._bar = "bar"
+
+    def tearDown(self):
+        self._bar = None
 
     def test_foo(self):
-        print("Testing foo")
-
-    def test_bar(self):
-        print("Testing bar")
+        pprint("Testing foo")
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
