faa350e279b2cd2c0811a6570fada8bcc3d68b2e
vduzh <vduzh@tut.by>
2024-04-23 15:57:02 +0300

    added test_typed_predictors

--- libs/dspy/signature_test.py
+++ libs/dspy/signature_test.py
@@ -2,6 +2,7 @@ import unittest
 from unittest import TestCase
 
 import dspy
+from pydantic import BaseModel, Field
 
 from model import get_lm
 
@@ -141,6 +142,46 @@ class TestSignature(TestCase):
 
         # assert the result
         self.assertFalse(eval(response.faithfulness))
+
+    def test_typed_predictors(self):
+        class Input(BaseModel):
+            context: str = Field(description="The context for the question")
+            query: str = Field(description="The question to be answered")
+
+        class Output(BaseModel):
+            answer: str = Field(description="The answer for the question")
+            confidence: float = Field(ge=0, le=1, description="The confidence score for the answer")
+
+        class QASignature(dspy.Signature):
+            """
+            Answer the question based on the context and query provided, and on the scale of 10 tell how confident
+            you are about the answer.
+            """
+
+            input: Input = dspy.InputField()
+            output: Output = dspy.OutputField()
+
+        # define typed predictor
+        predictor = dspy.TypedPredictor(QASignature)
+
+        # the same thing
+        # predictor = dspy.TypedPredictor("input:Input -> output:Output")
+
+        # testing out
+        doc_query_pair = Input(
+            context="The quick brown fox jumps over the lazy dog",
+            query="What does the fox jumps over?",
+        )
+
+        prediction = predictor(input=doc_query_pair)
+        output = prediction.output
+
+        # unpack
+        answer, confidence = output
+
+        print(f"Prediction: {prediction}\n")
+        print(f"Answer: {answer}, Answer Type: {type(answer)}")
+        print(f"Confidence Score: {confidence}, Confidence Score Type: {type(confidence)}")
 
 
 if __name__ == '__main__':
