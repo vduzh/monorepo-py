9becce9ef30c625d1748e74cd98c6a5f6e40a795
vduzh <vduzh@tut.by>
2024-04-23 15:28:34 +0300

    added a placeholder

---
+++ libs/dspy/assertions_test.py
@@ -0,0 +1,26 @@
+import unittest
+from pprint import pprint
+
+
+class TestAssertions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._foo = "foo"
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._foo = None
+
+    def setUp(self):
+        self._bar = "bar"
+
+    def tearDown(self):
+        self._bar = None
+
+    def test_foo(self):
+        pprint("Testing foo")
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
