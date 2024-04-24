2c9f1d93e5c7d98ec41e22176df9ca4313efea06
vduzh <vduzh@tut.by>
2024-04-24 14:51:27 +0300

    added test_retrieve

--- libs/dspy/retrieve_test.py
+++ libs/dspy/retrieve_test.py
@@ -1,26 +1,49 @@
 import unittest
-from pprint import pprint
+
+import dspy
+from dspy.datasets import HotPotQA
+
+from libs.dspy.model import get_lm
 
 
 class TestRetrieve(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls._foo = "foo"
+        # Set up the LM
+        lm = get_lm()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls._foo = None
+        # Set up the retrieval model
+        colbert_v2_wiki17_abstracts = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')
 
-    def setUp(self):
-        self._bar = "bar"
+        # Configure the DSPy
+        dspy.settings.configure(lm=lm, rm=colbert_v2_wiki17_abstracts)
 
-    def tearDown(self):
-        self._bar = None
+        # Load the dataset
+        dataset = HotPotQA(train_seed=1, train_size=20, eval_seed=2023, dev_size=50, test_size=0)
 
-    def test_foo(self):
-        # pprint("Testing foo")
-        # self.assertTrue(True)
-        raise NotImplementedError()
+        # Tell DSPy that the 'question' field is the input. Any other fields are labels and/or metadata.
+        cls.train_set = [x.with_inputs('question') for x in dataset.train]
+        cls.dev_set = [x.with_inputs('question') for x in dataset.dev]
+
+    def test_retrieve(self):
+        # Get an example
+        dev_example = self.dev_set[18]
+        question = dev_example.question
+
+        # Create a retriever
+        retrieve = dspy.Retrieve(k=3)
+
+        # Extract the data for the question
+        prediction = retrieve(question)
+        print("Prediction:", prediction)
+
+        # Get the passages from the prediction
+        top_k_passages = prediction.passages
+
+        # Print out the results
+        print(f"Top {retrieve.k} passages for question: \"{question}\" \n {'-' * 30}\n")
+        for i, passage in enumerate(top_k_passages):
+            print(f"{i}]: {passage}\n")
 
 
 if __name__ == '__main__':
