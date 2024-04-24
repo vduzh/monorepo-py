3d3ab87f6886597c402713d702a804d38023957f
vduzh <vduzh@tut.by>
2024-04-24 17:50:57 +0300

    added test_hot_pot_qa_data_set

--- libs/dspy/data_test.py
+++ libs/dspy/data_test.py
@@ -1,6 +1,10 @@
+import json
 import unittest
+from pprint import pprint
 
 import dspy
+from dspy import Example
+from dspy.datasets import HotPotQA
 
 
 class TestData(unittest.TestCase):
@@ -41,6 +45,45 @@ class TestData(unittest.TestCase):
             dspy.Example(report="Long report 2", summary="Short summary 2"),
         ]
         print(train_set)
+
+    def test_data_set(self):
+        raise NotImplementedError()
+
+    def test_hot_pot_qa_data_set(self):
+        def print_example(title: str, example: Example):
+            # use list comprehension to initialize a dictionary
+            # data = {key: example.get(key) for key in example.keys()}
+
+            # print(title, json.dumps(data, indent=2))
+            # print(title, data)
+            print(title, example)
+
+        # Load the dataset
+        data_set = HotPotQA(
+            train_seed=1,
+            train_size=20,
+            eval_seed=2023,
+            dev_size=50,
+            test_size=5,
+        )
+
+        print_example("data_set::train:example", data_set.train[0])
+        print_example("data_set::dev:example", data_set.dev[0])
+        print_example("data_set::test:example", data_set.test[0])
+
+        # Tell DSPy that the 'question' field is the input. Any other fields are labels and/or metadata.
+        train_set = [x.with_inputs('question') for x in data_set.train]
+        dev_set = [x.with_inputs('question') for x in data_set.dev]
+        test_set = [x.with_inputs('question') for x in data_set.test]
+
+        train_example = train_set[0]
+        print_example("Train example:", train_example)
+
+        dev_example = dev_set[0]
+        print_example("Dev example:", dev_example)
+
+        test_example = test_set[0]
+        print_example("Test example:", test_example)
 
 
 if __name__ == '__main__':
