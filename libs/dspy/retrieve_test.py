0c67288f4711eb2d533608f86deb6c35706293e7
vduzh <vduzh@tut.by>
2024-05-27 18:44:24 +0300

    reworked

--- libs/dspy/retrieve_test.py
+++ libs/dspy/retrieve_test.py
@@ -1,7 +1,6 @@
 import unittest
 
 import dspy
-from dspy.datasets import HotPotQA
 
 from libs.dspy.model import get_lm
 
@@ -13,27 +12,20 @@ class TestRetrieve(unittest.TestCase):
         lm = get_lm()
 
         # Set up the retrieval model
-        colbert_v2_wiki17_abstracts = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')
+        retrieval_model = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')
 
         # Configure the DSPy
-        dspy.settings.configure(lm=lm, rm=colbert_v2_wiki17_abstracts)
-
-        # Load the dataset
-        dataset = HotPotQA(train_seed=1, train_size=20, eval_seed=2023, dev_size=50, test_size=0)
-
-        # Tell DSPy that the 'question' field is the input. Any other fields are labels and/or metadata.
-        cls.train_set = [x.with_inputs('question') for x in dataset.train]
-        cls.dev_set = [x.with_inputs('question') for x in dataset.dev]
+        dspy.settings.configure(lm=lm, rm=retrieval_model)
 
     def test_retrieve(self):
-        # Get an example
-        dev_example = self.dev_set[18]
-        question = dev_example.question
+        # Question
+        question = """ Who conducts the draft in which Marc-Andre Fleury was drafted to the Vegas Golden Knights for 
+        the 2017-18 season?"""
 
         # Create a retriever
         retrieve = dspy.Retrieve(k=3)
 
-        # Extract the data for the question
+        # Search for the top-k passages that match a given query
         prediction = retrieve(question)
         print("Prediction:", prediction)
 
