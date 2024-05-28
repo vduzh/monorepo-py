afb3ac31e18272c31730c8e47cac29fedad709e8
vduzh <vduzh@tut.by>
2024-05-28 17:57:27 +0300

    added test_evaluate_compiled_program

--- libs/dspy/evaluate_test.py
+++ libs/dspy/evaluate_test.py
@@ -17,16 +17,35 @@ class TestEvaluate(unittest.TestCase):
 
         # Load math questions from the GSM8K dataset
         gsm8k = GSM8K()
-        gsm8k_train_set, cls.gsm8k_dev_set = gsm8k.train[:10], gsm8k.dev[:10]
+        cls.gsm8k_train_set, cls.gsm8k_dev_set = gsm8k.train[:10], gsm8k.dev[:10]
+
+    def test_evaluate_program(self):
+        # Create a program
+        program = SimpleProgram()
+
+        # Set up the evaluator, which can be used multiple times.
+        evaluate = Evaluate(
+            devset=self.gsm8k_dev_set,
+            metric=gsm8k_metric,
+            num_threads=4,
+            display_progress=True,
+            display_table=0
+        )
+
+        # Evaluate the `program`
+        evaluate(program)
+
+    def test_evaluate_compiled_program(self):
+        # Create a program
+        program = SimpleProgram()
 
         # Define the teleprompter and compile the program
         teleprompter = BootstrapFewShot(
             metric=gsm8k_metric,
             max_bootstrapped_demos=4,
             max_labeled_demos=4)
-        cls.compiled_program = teleprompter.compile(SimpleProgram(), trainset=gsm8k_train_set)
+        compiled_program = teleprompter.compile(program, trainset=self.gsm8k_train_set)
 
-    def test_evaluate_program(self):
         # Set up the evaluator, which can be used multiple times.
         evaluate = Evaluate(
             devset=self.gsm8k_dev_set,
@@ -37,7 +56,7 @@ class TestEvaluate(unittest.TestCase):
         )
 
         # Evaluate our `compiled_program`
-        evaluate(self.compiled_program)
+        evaluate(compiled_program)
 
 
 if __name__ == '__main__':
