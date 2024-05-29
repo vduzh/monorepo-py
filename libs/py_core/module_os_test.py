119528c0a35fedcd710d02439869787a939e45c9
vduzh <vduzh@tut.by>
2024-05-29 17:56:28 +0300

    added a couple of test

--- libs/py_core/module_os_test.py
+++ libs/py_core/module_os_test.py
@@ -3,9 +3,21 @@ import unittest
 
 
 class TestOs(unittest.TestCase):
-    def test_path(self):
+    def test_name(self):
         value = os.name
         print(value)
+
+    def test_path_abspath(self):
+        file_dir_name = os.path.abspath(__file__)
+        print("Abs path:", file_dir_name)
+
+    def test_path_dirname(self):
+        file_dir_name = os.path.dirname(__file__)
+        print("Dir name:", file_dir_name)
+
+    def test_path_basename(self):
+        base_name = os.path.basename(__file__)
+        print("Base name:", base_name)
 
     def test_urandom(self):
         value = os.urandom(10)
