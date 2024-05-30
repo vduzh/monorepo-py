2fdc9ee198e68d4334633f461c6f8b05ca497d9b
vduzh <vduzh@tut.by>
2024-05-30 13:02:17 +0300

    added MATH_QUESTION and MATH_ANSWER

--- libs/dspy/utils/constants.py
+++ libs/dspy/utils/constants.py
@@ -1,2 +1,10 @@
 QUESTION = "What is the capital of Germany?"
 ANSWER = "Berlin"
+
+MATH_QUESTION = """
+A rectangle has a length that is twice its width. If the perimeter of the rectangle is 36 meters, 
+what are the length of the rectangle? Give me the length as a digit.
+"""
+MATH_ANSWER = "12"
+
+
