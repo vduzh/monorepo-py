9b634f15349e0c5f41de90443a4d0669f26d3351
vduzh <vduzh@tut.by>
2024-05-31 11:50:51 +0300

    reworked

--- libs/dspy/metrics_test.py
+++ libs/dspy/metrics_test.py
@@ -1,17 +1,35 @@
 import unittest
 
 import dspy
-from dspy import Module
-from dspy.evaluate import answer_exact_match, answer_passage_match, Evaluate
+from dsp import Example
+from dspy.evaluate import answer_exact_match, answer_passage_match
+
+from libs.dspy.utils.constants import ANSWER, QUESTION
 
 
 class TestMetrics(unittest.TestCase):
 
-    def test_simple_metric(self):
+    def test_built_in_answer_exact_match(self):
+        example = Example(question=QUESTION, answer=ANSWER)
+        pred = Example(question=QUESTION, answer=ANSWER)
+        res = answer_exact_match(example, pred)
+
+        self.assertTrue(res)
+
+    def test_built_in_answer_passage_match(self):
+        # TODO: not implemented
+        example = Example(question=QUESTION, answer=ANSWER)
+        pred = Example(question=QUESTION, answer=ANSWER)
+        res = answer_passage_match(example, pred)
+
+        self.assertTrue(res)
+
+    def test_custom_simple_metric(self):
+        # Create a metric function that returns either a number or a boolean value
         def validate_answer(example, pred, trace=None):
             return example.answer.lower() == pred.answer.lower()
 
-    def test_complex_metric(self):
+    def test_custom_complex_metric(self):
         # Checks for multiple properties
         def validate_context_and_answer(example, pred, trace=None):
             # check the gold label and the predicted answer are the same
@@ -25,67 +43,30 @@ class TestMetrics(unittest.TestCase):
             else:  # if we're doing bootstrapping, i.e. self-generating good demonstrations of each step
                 return answer_match and context_match
 
-    def test_built_in_answer_exact_match(self):
-        answer_exact_match()
-        pass
+    def test_custom_simple_ai_based_metric(self):
+        class FactJudge(dspy.Signature):
+            """Judge if the answer is factually correct based on the context."""
 
-    def test_built_in_answer_passage_match(self):
-        answer_passage_match()
-        pass
+            context = dspy.InputField(desc="Context for the prediction")
+            question = dspy.InputField(desc="Question to be answered")
+            answer = dspy.InputField(desc="Answer for the question")
 
-    def test_simple_evaluation(self):
-        # development set
-        dev_set = [
-            dspy.Example(question="What is the capital of Germany?", answer="Berlin").with_inputs("question"),
-            dspy.Example(question="What is the capital of Great Britain?", answer="London").with_inputs("question"),
-        ]
+            factually_correct = dspy.OutputField(
+                desc="Is the answer factually correct based on the context?",
+                prefix="Factual[Yes/No]:"
+            )
 
-        # test program
-        class TestProgram(Module):
+        def metric(example, pred):
+            """ Asks LLM if the answer is factually correct based on the context."""
 
-            def forward(self, question):
-                if "Germany" in question:
-                    return dspy.Example(question=question, answer="Berlin")
-                else:
-                    return dspy.Example(question=question, answer="London")
+            # Use the ChainOfThought module to call LLM
+            judge = dspy.ChainOfThought(FactJudge)
 
-        program = TestProgram()
+            # Call LLM to judge
+            factual = judge(context=example.context, question=example.question, answer=pred.answer)
+            return int(factual == "Yes")
 
-        # simple metric
-        def metric(example, pred, trace=None):
-            return example.answer.lower() == pred.answer.lower()
-
-        # evaluate
-        scores = []
-        for e in dev_set:
-            pred = program(**e.inputs())
-            score = metric(e, pred)
-            scores.append(score)
-
-        print("scores", scores)
-
-    def test_evaluate(self):
-        dev_set = [
-            dspy.Example(question="What is the capital of Germany?", answer="Berlin").with_inputs("question"),
-            dspy.Example(question="What is the capital of Great Britain?", answer="London").with_inputs("question"),
-        ]
-
-        # test program
-        class TestProgram(Module):
-
-            def forward(self, question):
-                return dspy.Example(question="What is the capital of Germany?", answer="Berlin")
-
-        program = TestProgram()
-
-        # Set up the evaluator, which can be re-used in your code.
-        evaluator = Evaluate(devset=dev_set, num_threads=1, display_progress=True, display_table=5)
-
-        # Launch evaluation.
-        res = evaluator(program, metric=answer_exact_match)
-        print(res)
-
-    def test_evaluate_with_ai(self):
+    def test_custom_ai_based_metric(self):
         # Define the signature for automatic assessments.
         class Assess(dspy.Signature):
             """Assess the quality of a tweet along the specified dimension."""
@@ -109,7 +90,9 @@ class TestMetrics(unittest.TestCase):
             correct, engaging = [m.assessment_answer.lower() == 'yes' for m in [correct, engaging]]
             score = (correct + engaging) if correct and (len(tweet) <= 280) else 0
 
-            if trace is not None: return score >= 2
+            if trace is not None:
+                return score >= 2
+
             return score / 2.0
 
 
