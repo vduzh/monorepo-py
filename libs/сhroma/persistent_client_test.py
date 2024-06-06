37f5ed0b809874eefd8f22d537e956e46b0191a5
vduzh <vduzh@tut.by>
2024-06-06 18:44:38 +0300

    refactored

--- libs/сhroma/persistent_client_test.py
+++ libs/сhroma/persistent_client_test.py
@@ -5,7 +5,7 @@ import chromadb
 from chromadb import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
 
 
-class TestCPersistentClient(unittest.TestCase):
+class TestPersistentClient(unittest.TestCase):
     test_collection = "test_collection"
 
     @classmethod
@@ -18,17 +18,8 @@ class TestCPersistentClient(unittest.TestCase):
             database=DEFAULT_DATABASE,
         )
 
-    def setUp(self):
-        try:
-            self._persistent_client.delete_collection(self.test_collection)
-        except ValueError:
-            pass
-
     def test_create_collection(self):
-        collection = self._persistent_client.create_collection(name=self.test_collection)
-
-    def test_load_collection(self):
-        collection = self._persistent_client.get_or_create_collection(name=self.test_collection)
+        self._persistent_client.get_or_create_collection(name=self.test_collection)
 
 
 if __name__ == '__main__':
