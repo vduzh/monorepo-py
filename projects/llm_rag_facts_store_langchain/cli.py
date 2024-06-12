ae40fb588afc5f721276c49b3a4a4d1079bc0db0
vduzh <vduzh@tut.by>
2024-06-12 15:48:45 +0300

    update env keys

--- projects/llm_rag_facts_store_langchain/cli.py
+++ projects/llm_rag_facts_store_langchain/cli.py
@@ -16,10 +16,10 @@ def cli():
     # initialize the dependency injection container
     container = AppContainer()
     container.config.lm.embedding_model_name.from_env("OPENAI_API_EMBEDDING_MODEL", required=True)
-    container.config.lm.embeddings.from_env('EMBEDDINGS', default='openai')
-    container.config.store.chromadb_client.from_env('CHROMADB_CLIENT', default='in_memory')
-    container.config.store.chromadb_client_http_host.from_env('CHROMADB_HTTP_CLIENT_HOST', default='localhost')
-    container.config.store.chromadb_client_http_port.from_env('CHROMADB_HTTP_CLIENT_PORT', default="8000")
+    container.config.lm.embeddings.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_EMBEDDINGS', default='openai')
+    container.config.store.chromadb_client.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_CLIENT', default='in_memory')
+    container.config.store.chromadb_client_http_host.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_HOST', default='localhost')
+    container.config.store.chromadb_client_http_port.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_PORT', default="8000")
     container.wire(modules=[__name__])
     # pass
 
