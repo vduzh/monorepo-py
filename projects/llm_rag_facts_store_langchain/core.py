1ce512c073ed8d4ad7e8b787f48ec6ba6a020a21
vduzh <vduzh@tut.by>
2024-06-12 17:30:08 +0300

    refactored

--- projects/llm_rag_facts_store_langchain/core.py
+++ projects/llm_rag_facts_store_langchain/core.py
@@ -11,9 +11,11 @@ load_dotenv()
 def init_container():
     """Initialize the dependency injection container"""
 
+    prefix = "LLM_RAG_FACTS_STORE_LANGCHAIN"
+
     container = AppContainer()
     container.config.lm.embeddings.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_EMBEDDINGS',
+        f'{prefix}EMBEDDINGS',
         default='openai'
     )
     container.config.lm.openai.embedding_model_name.from_env(
@@ -21,37 +23,37 @@ def init_container():
         required=True
     )
     container.config.store.chromadb_client.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_CLIENT',
+        f'{prefix}CHROMADB_CLIENT',
         # in_memory, persistent and http supported
         default='in_memory'
     )
     container.config.store.chromadb_client_http_host.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_HOST',
+        f'{prefix}CHROMADB_HTTP_CLIENT_HOST',
         default='localhost'
     )
     container.config.store.chromadb_client_http_port.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_PORT',
+        f'{prefix}CHROMADB_HTTP_CLIENT_PORT',
         default=8000)
     container.config.services.documents_service.text_splitter.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_TEXT_SPLITTER',
+        f'{prefix}DOCUMENTS_SERVICE_TEXT_SPLITTER',
         # character_text supported
         default='character_text'
     )
     container.config.services.documents_service.character_text_splitter_chunk_size.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_SIZE',
+        f'{prefix}DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_SIZE',
         default=201
     )
     container.config.services.documents_service.character_text_splitter_chunk_overlap.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_OVERLAP',
+        f'{prefix}DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_OVERLAP',
         default=0
     )
     container.config.services.documents_service.text_loader.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_TEXT_LOADER',
+        f'{prefix}DOCUMENTS_SERVICE_TEXT_LOADER',
         # local and external supported
         default='local'
     )
     container.config.services.documents_service.external_text_loader_path.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_EXTERNAL_TEXT_LOADER_PATH',
+        f'{prefix}DOCUMENTS_SERVICE_EXTERNAL_TEXT_LOADER_PATH',
         default=os.path.join(os.path.expanduser('~'), "llm_rag_facts_store_langchain", "documents", "facts_2.txt")
     )
 
