5374cf7b9baf2c31cdc67ac4913e52e114d71058
vduzh <vduzh@tut.by>
2024-06-12 16:39:00 +0300

    refactored

--- projects/llm_rag_facts_store_langchain/cli.py
+++ projects/llm_rag_facts_store_langchain/cli.py
@@ -1,3 +1,5 @@
+import os
+
 import click
 from dependency_injector.wiring import Provide, inject
 from dotenv import load_dotenv
@@ -15,25 +17,48 @@ def cli():
 
     # initialize the dependency injection container
     container = AppContainer()
-    container.config.lm.embeddings.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_EMBEDDINGS', default='openai')
-    container.config.lm.openai.embedding_model_name.from_env("OPENAI_API_EMBEDDING_MODEL", required=True)
-    container.config.store.chromadb_client.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_CLIENT',
-                                                    default='in_memory')
-    container.config.store.chromadb_client_http_host.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_HOST',
-                                                              default='localhost')
-    container.config.store.chromadb_client_http_port.from_env('LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_PORT',
-                                                              default=8000)
+    container.config.lm.embeddings.from_env(
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_EMBEDDINGS',
+        default='openai'
+    )
+    container.config.lm.openai.embedding_model_name.from_env(
+        "OPENAI_API_EMBEDDING_MODEL",
+        required=True
+    )
+    container.config.store.chromadb_client.from_env(
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_CLIENT',
+        # in_memory, persistent and http supported
+        default='in_memory'
+    )
+    container.config.store.chromadb_client_http_host.from_env(
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_HOST',
+        default='localhost'
+    )
+    container.config.store.chromadb_client_http_port.from_env(
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_CHROMADB_HTTP_CLIENT_PORT',
+        default=8000)
     container.config.services.documents_service.text_splitter.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_TEXT_SPLITTER', default='character_text')
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_TEXT_SPLITTER',
+        # character_text supported
+        default='character_text'
+    )
     container.config.services.documents_service.character_text_splitter_chunk_size.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_SIZE', default=201)
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_SIZE',
+        default=201
+    )
     container.config.services.documents_service.character_text_splitter_chunk_overlap.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_OVERLAP', default=0)
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_CHARACTER_TEXT_SPLITTER_CHUNK_OVERLAP',
+        default=0
+    )
     container.config.services.documents_service.text_loader.from_env(
-        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_TEXT_LOADER', default='local')
+        'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_TEXT_LOADER',
+        # local and external supported
+        default='local'
+    )
     container.config.services.documents_service.external_text_loader_path.from_env(
         'LLM_RAG_FACTS_STORE_LANGCHAIN_DOCUMENTS_SERVICE_EXTERNAL_TEXT_LOADER_PATH',
-        default="d:\\documents\\new_facts.txt")
+        default=os.path.join(os.path.expanduser('~'), "llm_rag_facts_store_langchain", "documents", "facts_2.txt")
+    )
     container.wire(modules=[__name__])
     # pass
 
