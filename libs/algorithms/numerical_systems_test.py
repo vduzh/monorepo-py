c35246e765b34f1171c932a76ace4b3a3383ae61
vduzh <vduzh@tut.by>
2024-09-23 12:52:44 +0300

    added test_generate

--- libs/algorithms/numerical_systems_test.py
+++ libs/algorithms/numerical_systems_test.py
@@ -14,6 +14,19 @@ class TestNumericalSystems(unittest.TestCase):
         self.assertEqual("1100100", convert(100, 2))
         self.assertEqual("210154", convert(10996, 16))
 
+    def test_oter_to_decimal(self):
+        def convert(value: str, base: int) -> str:
+            res = 0
+            reversed_value = value[::-1]
+            for i in range(len(reversed_value)):
+                res += int(reversed_value[i]) * base ** i
+            return res
+
+        self.assertEqual(0, convert("0", 2))
+        self.assertEqual(1, convert("01", 2))
+        self.assertEqual(2, convert("10", 2))
+        self.assertEqual(3, convert("11", 2))
+
     def test_decimal_to_binary_ones_count(self):
         def ones_count(value: int, base: int = 2) -> int:
             count = 0
@@ -38,3 +51,20 @@ class TestNumericalSystems(unittest.TestCase):
 
         self.assertEqual(90, diff(239, 8))
         self.assertEqual(-34, diff(1000000000, 7))
+
+    def test_bit_reverse(self):
+
+        def b(value: int, base: int = 2) -> str:
+            bits = ""
+            while value > 0:
+                bits += str(value % base)
+                value = value // base
+
+            res = 0
+            for i in range(len(bits)):
+                value = bits[len(bits) - 1 - i]
+                res += int(value) * base ** i
+            return res
+
+        self.assertEqual(1, b(4))
+        self.assertEqual(3, b(6))
