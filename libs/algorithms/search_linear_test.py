87087e568bda4002bb65429feb471d94261c742b
vduzh <vduzh@tut.by>
2024-09-30 17:29:50 +0300

    added

---
+++ libs/algorithms/search_linear_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+DATA = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+    13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+    37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
+    49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,
+    73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+    85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96,
+    97, 98, 99, 100
+]
+
+
+class TestLinearSearch(unittest.TestCase):
+
+    def test_linear(self):
+        def linear_search(numbers: list[int], target: int):
+            ind = -1
+            for i in range(len(numbers)):
+                if numbers[i] == target:
+                    ind = i
+                    break
+            return ind
+
+        self.assertEqual(86, linear_search(DATA, 87))
+        self.assertEqual(10, linear_search(DATA, 11))
