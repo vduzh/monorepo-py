dcc29bce0e2874e55aeb4ebac828b628934d1fdb
vduzh <vduzh@tut.by>
2024-10-07 18:14:37 +0300

    added comments

--- libs/algorithms/sort_quick.py
+++ libs/algorithms/sort_quick.py
@@ -8,13 +8,21 @@ class QuickSort(unittest.TestCase):
             if len(lst) <= 1:
                 return lst
 
+            # calculate the medium value
             mid = lst[len(lst) // 2]
 
+            # find all the values that less than mid
             less = [i for i in lst if i < mid]
+            # find all the values that equal to mid
             equal = [i for i in lst if i == mid]
+            # find all the values that greater than mid
             greater = [i for i in lst if i > mid]
 
-            return quick_sort(less) + equal + quick_sort(greater)
+            return (
+                    quick_sort(less) +  # sort the 'less' values
+                    equal +  # add the equal values to the sorted 'less' values
+                    quick_sort(greater)  # add the sorted 'greater' values
+            )
 
         self.assertEqual([1, 2, 3, 4], quick_sort([1, 3, 4, 2]))
         self.assertEqual([2, 5, 7], quick_sort([7, 2, 5]))
