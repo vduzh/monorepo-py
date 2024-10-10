d215af3f38931bf25849a1a97b1a03d605327352
vduzh <vduzh@tut.by>
2024-10-10 13:51:09 +0300

    added test_combinations_salads

--- libs/algorithms/test/combinatorics_test.py
+++ libs/algorithms/test/combinatorics_test.py
@@ -61,3 +61,46 @@ class CombinatoricsTest(unittest.TestCase):
             # end of the task
 
             self.assertEqual(expected, res)
+
+    def test_arrangements_hockey(self):
+        data_set = [
+            (3, 6),
+            (20, 6840),
+        ]
+
+        for data in data_set:
+            n, expected = data
+
+            # start of the task
+            # n = int(input(""))
+            k = 3
+
+            res = factorial(n) // factorial(n - k)
+            print(res)
+            # end of the task
+
+            self.assertEqual(expected, res)
+
+    def test_combinations_salads(self):
+        """
+        Task: https://acmp.ru/asp/do/index.asp?main=task&id_course=2&id_section=16&id_topic=21&id_problem=106
+        """
+        data_set = [
+            (3, 4),
+            (4, 11),
+        ]
+
+        for data in data_set:
+            n, expected = data
+
+            # start of the task
+            # n = int(input(""))
+
+            res = 0
+            for k in range(2, n + 1):
+                combinations = factorial(n) // (factorial(n - k) * factorial(k))
+                res += combinations
+            print(res)
+            # end of the task
+
+            self.assertEqual(expected, res)
